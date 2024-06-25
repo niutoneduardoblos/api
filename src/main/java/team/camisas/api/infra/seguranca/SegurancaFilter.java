ba1f5f8d5f38e39e977e8ce5607c78acb3d01afb
Niuton Eduardo Blos <niuton@outlook.com>
2024-06-25 20:50:59 -0300

    Validação de token no cabeçalho iniciada

---
+++ src/main/java/team/camisas/api/infra/seguranca/SegurancaFilter.java
@@ -0,0 +1,33 @@
+package team.camisas.api.infra.seguranca;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class SegurancaFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        var tokenJWT = recuperarToken(request);
+        System.out.println(tokenJWT);
+        filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader == null) {
+            throw new RuntimeException("Token JWT não enviado no cabeçalho");
+        }
+        return authorizationHeader.replace("Bearer ", "");
+    }
+
+}
