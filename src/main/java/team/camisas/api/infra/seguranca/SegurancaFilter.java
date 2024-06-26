a221ad70fa03eadcc1b77f0c7e8987d60aef3663
Niuton Eduardo Blos <niuton@outlook.com>
2024-06-26 20:36:53 -0300

    Finalização uso de token para requisições

--- src/main/java/team/camisas/api/infra/seguranca/SegurancaFilter.java
+++ src/main/java/team/camisas/api/infra/seguranca/SegurancaFilter.java
@@ -2,6 +2,9 @@ package team.camisas.api.infra.seguranca;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -9,25 +12,39 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import team.camisas.api.domain.usuario.UsuarioRepository;
 
 @Component
 public class SegurancaFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
         var tokenJWT = recuperarToken(request);
-        System.out.println(tokenJWT);
+
+        if (tokenJWT != null) {
+            var subject = tokenService.getSubject(tokenJWT);
+            var usuarioBean = usuarioRepository.findByEmail(subject);
+            var authentication = new UsernamePasswordAuthenticationToken(usuarioBean, null, usuarioBean.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
         filterChain.doFilter(request, response);
     }
 
     private String recuperarToken(HttpServletRequest request) {
         var authorizationHeader = request.getHeader("Authorization");
-        if (authorizationHeader == null) {
-            throw new RuntimeException("Token JWT não enviado no cabeçalho");
+        if (authorizationHeader != null) {
+            return authorizationHeader.replace("Bearer ", "");
         }
-        return authorizationHeader.replace("Bearer ", "");
+        return null;
     }
 
 }
--- src/main/java/team/camisas/api/infra/seguranca/TokenService.java
+++ src/main/java/team/camisas/api/infra/seguranca/TokenService.java
@@ -4,12 +4,16 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
+import javax.management.RuntimeErrorException;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 
 import team.camisas.api.domain.usuario.UsuarioBean;
 
@@ -25,10 +29,23 @@ public class TokenService {
             return JWT.create()
                     .withIssuer("API colecao.camisas")
                     .withSubject(usuarioBean.getEmail())
-                    .withExpiresAt(dataExpiracao())
+                    .withExpiresAt(this.dataExpiracao())
                     .sign(algoritmo);
         } catch (JWTCreationException exception) {
             throw new RuntimeException("Erro ao gerar token JWT", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API colecao.camisas")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT invalido ou expirado!");
         }
     }
 
